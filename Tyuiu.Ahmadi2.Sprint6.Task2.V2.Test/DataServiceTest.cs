484cc71eb456379d527ced5c569e8947ea46c3fa
Ahmadi1 <Ahmadi@DS10BK3>
2025-12-10 01:19:27 -0800

    Task2

---
+++ Tyuiu.Ahmadi2.Sprint6.Task2.V2.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.Ahmadi2.Sprint6.Task2.V2.Lib;
+
+namespace Tyuiu.Ahmadi2.Sprint6.Task2.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double[] res = ds.GetMassFunction(startValue, stopValue);
+            double[] wait = {
+                20.15, 16.19, 10.81, 7.84, 4.81, 0, -1.97, -7.24, -12.01, -16.95, -21.51
+            };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.Ahmadi2.Sprint6.Task2.V2/FormMain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+using Tyuiu.Ahmadi2.Sprint6.Task2.V2.Lib;
+
+namespace Tyuiu.Ahmadi2.Sprint6.Task2.V2
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+
+            double[] values = ds.GetMassFunction(startValue, stopValue);
+
+            // پاک کردن داده‌های قبلی
+            dataGridViewResult.Rows.Clear();
+            dataGridViewResult.ColumnCount = 2;
+            dataGridViewResult.Columns[0].HeaderText = "x";
+            dataGridViewResult.Columns[1].HeaderText = "F(x)";
+
+            // پر کردن DataGridView
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                dataGridViewResult.Rows.Add(x.ToString(), values[count].ToString("F2"));
+                count++;
+            }
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 2 выполнил студент группы ИИПб-23-1 Ахмади Мухаммад Фархад", "Справка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
