8398c03459f42704c87634a6cc78b61102401fe2
Ahmadi1 <Ahmadi@DS10BK3>
2025-12-10 03:23:01 -0800

    Task6

---
+++ Tyuiu.Ahmadi2.Sprint6.Task6.V7.Lib/DataService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.Ahmadi2.Sprint6.Task6.V7.Lib
+{
+    public class DataService : ISprint6Task6V7
+    {
+        public string CollectTextFromFile(string path)
+        {
+            string result = "";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(path);
+
+                foreach (string line in lines)
+                {
+                    
+                    string[] words = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    
+                    if (words.Length >= 3)
+                    {
+                        result += words[2] + " ";
+                    }
+                }
+
+                
+                if (result.Length > 0)
+                {
+                    result = result.TrimEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ошибка при чтении файла: " + ex.Message);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.Ahmadi2.Sprint6.Task6.V7.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.Ahmadi2.Sprint6.Task6.V7.Lib;
+using System.IO;
+
+namespace Tyuiu.Ahmadi2.Sprint6.Task6.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCollectTextFromFile()
+        {
+            DataService ds = new DataService();
+
+            
+            string path = @"C:\Temp\InPutFileTask6V7.txt";
+            string[] testData =
+            {
+                "Это первая строка с несколькими словами",
+                "Вторая строка тоже имеет слова",
+                "Третья строка текста для проверки",
+                "Четвертая",
+                "Пятая строка с достаточным количеством"
+            };
+            File.WriteAllLines(path, testData);
+
+            string res = ds.CollectTextFromFile(path);
+            string wait = "строка строка с"; 
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
